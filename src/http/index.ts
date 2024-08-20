b9f0ca5f51ec3569c7d929fd8e05c831203f5e95
vladosik1998 <vladosik4891@gmail.com>
2024-08-20 22:14:11 +0300

    abracadabra

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,18 +1,21 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
-const baseURL = "https://adm.incasinowetrust.com/api/v1/"
+const baseURL = "https://adm.incasinowetrust.com/api/v1/";
+// const baseURL = "http://localhost:5000/api/"
 
 const $api = axios.create({
     withCredentials: true,
-    baseURL: `${baseURL}`,
-})
+    baseURL: baseURL, 
+});
 
 $api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-    config.headers.Authorization = `Basic aWdhcHA6VGNIQUtpVE9MZUNo`
-    config.headers.Accept = "application/json"
-    config.headers.Cookie = "sessionid=x4xrd3idqyutx6o6cooa2iwrv8myd447"
-    return config
-})
+    config.headers.Authorization = `Basic aWdhcHA6VGNIQUtpVE9MZUNo`;
+    config.headers.Accept = "application/json";
+    config.headers.Cookie = "sessionid=1gzx5dk5bahkpztdsr27k65lvbkb5fkb";
+     config.headers['X-CSRF-Token'] = 'jFRZKSLSi4CB38uR5pWaXQ5FU1X4rAfm;';
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 
-export default $api
-
+export default $api;
