82137c6dc54107cb408dab9500f296991480c406
vladosik1998 <vladosik4891@gmail.com>
2024-08-22 19:24:36 +0300

    delete api key

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,7 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 
 const baseURL = "https://adm.incasinowetrust.com/api/v1/";
 
-let API_KEY = '8f5d3f9e-2d7b-4b8f-a7c1-9c4e8b12a6e3';
+
 const $api = axios.create({
  //   withCredentials: true,
     baseURL: baseURL, 
@@ -10,7 +10,7 @@ const $api = axios.create({
 });
 
 $api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-     config.headers.Authorization = API_KEY;
+     config.headers.Authorization = '8f5d3f9e-2d7b-4b8f-a7c1-9c4e8b12a6e3';
     config.headers.Accept = "application/json";
     // config.headers.Cookie = "sessionid=1gzx5dk5bahkpztdsr27k65lvbkb5fkb";
     //  config.headers['X-CSRF-Token'] = 'jFRZKSLSi4CB38uR5pWaXQ5FU1X4rAfm;';
