f82fbb78826abd94106fe757c952cd862c2e0897
vladosik1998 <vladosik4891@gmail.com>
2024-09-13 11:49:33 +0300

    fix blur

--- src/page/SimpleBonus/index.tsx
+++ src/page/SimpleBonus/index.tsx
@@ -608,79 +608,70 @@ export const SimpleBonus = () => {
                     <SimpleBonusEssentialPrograms />
 
                     <section className="simple-bonus__casino-person casino-person">
-                        <div className="casino-person__container container">
-                            <div className="casino-person__body">
-                                <div className="casino-person__row">
-                                    <div className="casino-person__info info-casino-person">
-                                        <div className="info-casino-person__img">
-                                            <LazyLoadImage
-                                                src={harryStyles}
-                                                alt="HARRY STYLES"
-                                                
-                                            />
-                                        </div>
-                                        <div className="info-casino-person__content">
-                                            <h3 className="info-casino-person__name h3">
-                                                HARRY STYLES
-                                            </h3>
-                                            <div className="info-casino-person__position">
-                                                Content Maker, Chief Marketing
-                                                at Lerio
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="casino-person__socials">
-                                        <div className="socials-top-footer__items">
-                                            <div className="socials-top-footer__item">
-                                                <a
-                                                    rel="nofollow noopener"
-                                                    href=""
-                                                    aria-label="Put your description here."
-                                                    target="_blank"
-                                                    className="socials-top-footer__link"
-                                                >
-                                                    <svg>
-                                                        <use xlinkHref="#x"></use>
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                            <div className="socials-top-footer__item">
-                                                <a
-                                                    rel="nofollow noopener"
-                                                    href=""
-                                                    aria-label="Put your description here."
-                                                    target="_blank"
-                                                    className="socials-top-footer__link"
-                                                >
-                                                    <svg>
-                                                        <use xlinkHref="#facebook"></use>
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                            <div className="socials-top-footer__item">
-                                                <a
-                                                    rel="nofollow noopener"
-                                                    href=""
-                                                    aria-label="Put your description here."
-                                                    target="_blank"
-                                                    className="socials-top-footer__link"
-                                                >
-                                                    <svg>
-                                                        <use xlinkHref="#linkedin"></use>
-                                                    </svg>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="casino-person__waves">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+      <div className="casino-person__container container">
+      <div className="iwild-review__item item-iwild-review item-iwild-review_author">
+      <div className="item-iwild-review__content content-item-iwild-review">
+        <div className="content-item-iwild-review__image">
+          <LazyLoadImage src={harryStyles} alt="HARRY STYLES" />
+        </div>
+        <div className="content-item-iwild-review__body">
+          <div className="content-item-iwild-review__label">Author</div>
+          <div className="content-item-iwild-review__name">HARRY STYLES</div>
+          <div className="content-item-iwild-review__text">
+            <p>
+              Marketing @Coinmooner. Current learning project: USA.
+              <br />
+              I also summarise books on my <span>personal blog</span>.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="item-iwild-review__socials">
+        <div className="socials-top-footer__items">
+          <div className="socials-top-footer__item">
+            <a
+              href=""
+              aria-label="Put your description here."
+              target="_blank"
+              rel="nofollow noopener"
+              className="socials-top-footer__link"
+            >
+              <svg>
+                <use xlinkHref="#x"></use>
+              </svg>
+            </a>
+          </div>
+          <div className="socials-top-footer__item">
+            <a
+              href=""
+              aria-label="Put your description here."
+              target="_blank"
+              rel="nofollow noopener"
+              className="socials-top-footer__link"
+            >
+              <svg>
+                <use xlinkHref="#facebook"></use>
+              </svg>
+            </a>
+          </div>
+          <div className="socials-top-footer__item">
+            <a
+              href=""
+              aria-label="Put your description here."
+              target="_blank"
+              rel="nofollow noopener"
+              className="socials-top-footer__link"
+            >
+              <svg>
+                <use xlinkHref="#linkedin"></use>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+      </div>
+    </section>
                     <section className="main-gamble__bottom-filter-tags bottom-filter-tags">
                         <div className="bottom-filter-tags__container container">
                             <div className="bottom-filter-tags__top top">
