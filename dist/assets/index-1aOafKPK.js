1b34c63e038523a1e2232a8faf586b0cf7ff6ddd
vladosik1998 <vladosik4891@gmail.com>
2024-11-15 17:02:33 +0200

    fix ibg filter bonus

--- dist/assets/index-1aOafKPK.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DqeTmAIw.js";function i(){return document.title="Filter Loyalties",t.jsx("div",{children:"index"})}export{i as default};
---
+++ dist/assets/index-CvgIcM6-.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BTw3PvOy.js";function i(){return document.title="Filter Loyalties",t.jsx("div",{children:"index"})}export{i as default};
