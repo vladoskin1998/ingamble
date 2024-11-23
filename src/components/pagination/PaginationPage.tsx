138d5c1465515b67adc16cc0eb098fec020dfdd8
vladosik1998 <vladosik4891@gmail.com>
2024-11-23 14:35:47 +0200

    pagination next prev

--- src/components/pagination/PaginationPage.tsx
+++ src/components/pagination/PaginationPage.tsx
@@ -13,50 +13,47 @@ export const PaginationPage = ({
     countPageElem?: number
 }) => {
 
-    console.log(currentPage,countElem,currentPage,);
+    console.log(currentPage, countElem, currentPage);
     
     const totalPages = Math.ceil(countElem / countPageElem)
     
     const handlePageChange = (selectedItem: { selected: number }) => {
         const newPage = selectedItem.selected + 1
-        
         setCurrentPage(newPage)
     }
     
-    console.log(countElem, "===",countPageElem);
+    console.log(countElem, "===", countPageElem);
     
- 
-
     return (
         <>
-        {
-            countElem <= currentPage || <button
-            onClick={(e) => {
-                e.preventDefault()
-                if (currentPage < totalPages)
-                    setCurrentPage(currentPage + 1)
-            }}
-            className="main-loyaltie-programs__btn-more"
-        >
-            Show More
-        </button>
-        }
-            
+            {
+                countElem <= currentPage || <button
+                    onClick={(e) => {
+                        e.preventDefault()
+                        if (currentPage < totalPages)
+                            setCurrentPage(currentPage + 1)
+                    }}
+                    className="main-loyaltie-programs__btn-more"
+                >
+                    Show More
+                </button>
+            }
+
             <ReactPaginate
-                previousLabel={
+                previousLabel={currentPage > 1 ? (
                     <button>
                         <svg>
                             <use xlinkHref="#arrow-btn"></use>
                         </svg>
                     </button>
-                }
-                nextLabel={
+                ) : null}
+                nextLabel={currentPage < totalPages ? (
                     <button>
                         <svg>
                             <use xlinkHref="#arrow-btn"></use>
                         </svg>
                     </button>
-                }
+                ) : null}
                 breakLabel="..."
                 breakClassName="pages-pagination__page-numbers dots"
                 pageCount={totalPages}
@@ -66,10 +63,18 @@ export const PaginationPage = ({
                 containerClassName="main-loyaltie-programs__pagination pagination"
                 pageClassName="pages-pagination__page-numbers"
                 pageLinkClassName="pagination__link"
-                previousClassName="pagination__btn pagination__btn_prev"
-                nextClassName="pagination__btn pagination__btn_next"
+                previousClassName={
+                    currentPage > 1 
+                        ? "pagination__btn pagination__btn_prev" 
+                        : ""
+                }
+                nextClassName={
+                    currentPage < totalPages 
+                        ? "pagination__btn pagination__btn_next" 
+                        : ""
+                }
                 activeClassName="current"
-                forcePage={currentPage - 1} 
+                forcePage={currentPage - 1}
             />
         </>
     )
