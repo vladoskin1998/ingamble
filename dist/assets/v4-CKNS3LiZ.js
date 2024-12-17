f1abc7104a888ddfab4b6c200a97e5a21b99902f
vladosik1998 <vladosik4891@gmail.com>
2024-12-17 18:28:00 +0200

    fix icon mobile

--- dist/assets/v4-CKNS3LiZ.js
+++ dist/assets/v4-CKNS3LiZ.js
@@ -1,4 +1,4 @@
-import{z as re}from"./index-Jk8I6Vlo.js";var je={exports:{}};/**
+import{z as re}from"./index-Dz6Y14vZ.js";var je={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
