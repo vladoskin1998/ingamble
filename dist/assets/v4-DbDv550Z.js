77103bf7a0c0e0d8d624f291a8a887af9e539dbd
vladosik1998 <vladosik4891@gmail.com>
2024-12-17 20:02:15 +0200

    fix logo and pos

--- dist/assets/v4-DbDv550Z.js
+++ dist/assets/v4-DbDv550Z.js
@@ -1,4 +1,4 @@
-import{z as re}from"./index-C2_JZaE_.js";var je={exports:{}};/**
+import{z as re}from"./index-BVBkzYnr.js";var je={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
