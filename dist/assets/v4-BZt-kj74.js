3805d3515f8826df1a13cf43080496b3e60532fc
vladosik1998 <vladosik4891@gmail.com>
2024-12-17 20:06:36 +0200

    fix center loading 1

--- dist/assets/v4-BZt-kj74.js
+++ dist/assets/v4-BZt-kj74.js
@@ -1,4 +1,4 @@
-import{z as re}from"./index-CjMFWrUY.js";var je={exports:{}};/**
+import{z as re}from"./index-Lf-X1X52.js";var je={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
