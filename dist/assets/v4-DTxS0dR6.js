aa73574476dd00f980d03f48df835355621b5206
vladosik1998 <vladosik4891@gmail.com>
2024-12-17 20:42:00 +0200

    fix casino shad rev

--- dist/assets/v4-DTxS0dR6.js
+++ dist/assets/v4-DTxS0dR6.js
@@ -1,4 +1,4 @@
-import{z as re}from"./index-B2jG0XF0.js";var je={exports:{}};/**
+import{z as re}from"./index-mzoBUDGH.js";var je={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
