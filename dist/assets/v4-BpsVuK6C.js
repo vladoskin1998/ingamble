049a830bb02b38fb3b5fcacb3a4d3bd01b3457ed
vladosik1998 <vladosik4891@gmail.com>
2024-12-24 19:56:49 +0200

    add logo loader time

--- dist/assets/v4-BpsVuK6C.js
+++ dist/assets/v4-BpsVuK6C.js
@@ -1,4 +1,4 @@
-import{B as re}from"./index-LWCmdtKy.js";var je={exports:{}};/**
+import{B as re}from"./index-Bstqaxz3.js";var je={exports:{}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
