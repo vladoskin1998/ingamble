b7a4f84aa5afadba46f19da69811a26eda4553c5
vladosik1998 <vladosik4891@gmail.com>
2024-12-26 15:57:45 +0200

    del flex

--- dist/assets/BreadCrumb-CC7REqST.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CNHuPqHD.js";const a=t=>r.jsx(r.Fragment,{});export{a as B};
---
+++ dist/assets/BreadCrumb-mtM-OS4t.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D_CEI7qC.js";const a=t=>r.jsx(r.Fragment,{});export{a as B};
