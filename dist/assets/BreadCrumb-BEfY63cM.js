10d7405bc2cd6d29b5979dc2cd7d10658dd700b7
vladosik1998 <vladosik4891@gmail.com>
2024-12-30 14:56:10 +0200

    add box style 1

---
+++ dist/assets/BreadCrumb-BEfY63cM.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CP2O5Bpo.js";const a=t=>r.jsx(r.Fragment,{});export{a as B};
--- dist/assets/BreadCrumb-D0KY-Qxu.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-C4T5OK3j.js";const a=t=>r.jsx(r.Fragment,{});export{a as B};
