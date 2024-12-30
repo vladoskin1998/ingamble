aac8dcc76d83353bd97c246f587ee344785c4176
vladosik1998 <vladosik4891@gmail.com>
2024-12-30 15:31:41 +0200

    back to start pag

--- dist/assets/BreadCrumb-CWUzu9Vf.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-yyE7bgY4.js";const a=t=>r.jsx(r.Fragment,{});export{a as B};
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -5,27 +5,24 @@ import { Categories } from '../../components/categories/Categories'
 import { useQuery } from 'react-query'
 import { LogoLoader } from '../../components/loader/LogoLoader'
 import $api from '../../http'
-//@ts-ignore
 import { BlockTypeNumber, HomeDataBlock, HomeDataBlockMobile } from '../../types'
-import HomeMob from './HomeMob'
-// import HomePc from './HomePc'
- const HomePc = lazy(() => import('./HomePc'))
-// const BlockType1 = lazy(() => import('./BlockType1'))
-// const BlockType2Mobile = lazy(() => import('./BlockType2Mobile'))
-// const BlockType3Mobile = lazy(() => import('./BlockType3Mobile'))
-// const BlockType4Mobile = lazy(() => import('./BlockType4Mobile'))
-// const BlockType5Mobile = lazy(() => import('./BlockType5Mobile'))
-// const BlockType6 = lazy(() => import('./BlockType6'))
-// const BlockType7Mobile = lazy(() => import('./BlockType7Mobile'))
-// const BlockType9 = lazy(() => import('./BlockType9'))
-// const BlockType8 = lazy(() => import('./BlockType8'))
-// const BlockType2 = lazy(() => import('./BlockType2'))
-// const BlockType3 = lazy(() => import('./BlockType3'))
-// const BlockType4 = lazy(() => import('./BlockType4'))
-// const BlockType7 = lazy(() => import('./BlockType7'))
-// const BlockType5 = lazy(() => import('./BlockType5'))
-// const BlockMType2M = lazy(() => import('./BlockMType2M'))
-// const BlockMType3M = lazy(() => import('./BlockMType3M'))
+
+const BlockType1 = lazy(() => import('./BlockType1'))
+const BlockType2Mobile = lazy(() => import('./BlockType2Mobile'))
+const BlockType3Mobile = lazy(() => import('./BlockType3Mobile'))
+const BlockType4Mobile = lazy(() => import('./BlockType4Mobile'))
+const BlockType5Mobile = lazy(() => import('./BlockType5Mobile'))
+const BlockType6 = lazy(() => import('./BlockType6'))
+const BlockType7Mobile = lazy(() => import('./BlockType7Mobile'))
+const BlockType9 = lazy(() => import('./BlockType9'))
+const BlockType8 = lazy(() => import('./BlockType8'))
+const BlockType2 = lazy(() => import('./BlockType2'))
+const BlockType3 = lazy(() => import('./BlockType3'))
+const BlockType4 = lazy(() => import('./BlockType4'))
+const BlockType7 = lazy(() => import('./BlockType7'))
+const BlockType5 = lazy(() => import('./BlockType5'))
+const BlockMType2M = lazy(() => import('./BlockMType2M'))
+const BlockMType3M = lazy(() => import('./BlockMType3M'))
 const MoreBonusesForYourChoise = lazy(() => import('./MoreBonusesForYourChoise'))
 const CheckMoreWhatSuitsYouBest = lazy(() => import('../../components/categories/CheckMoreWhatSuitsYouBest'))
 const SubscribeForm = lazy(() => import('../../components/subscribe/SubscribeForm'))
@@ -44,38 +41,38 @@ const getHomeDataFetch = async () => {
     }
 }
 
-// const renderBlock = (block: any, index: number, isMobile: boolean) => {
-//     const lazyLoadImg: LazyImgHomeType = index < 3 ? 'eager' : 'lazy'
+const renderBlock = (block: any, index: number, isMobile: boolean) => {
+    const lazyLoadImg: LazyImgHomeType = index < 3 ? 'eager' : 'lazy'
 
-//     switch (block.items_block.type_block) {
-//         case BlockTypeNumber.BlockType1:
-//             return <BlockType1 data={block} />
-//         case BlockTypeNumber.BlockType9:
-//             return <BlockType9 data={block} />
-//         case BlockTypeNumber.BlockType2M:
-//             return <BlockMType2M data={block} lazyLoadImg={lazyLoadImg} />
-//         case BlockTypeNumber.BlockType3M:
-//             return <BlockMType3M data={block} />
-//         case BlockTypeNumber.BlockType6:
-//             return <BlockType6 data={block} />
-//         case BlockTypeNumber.BlockType8:
-//             return <BlockType8 data={block} />
-//         case BlockTypeNumber.BlockType2:
-//             return <>{isMobile ? <BlockType2Mobile data={block} /> : <BlockType2 data={block} />}</>
+    switch (block.items_block.type_block) {
+        case BlockTypeNumber.BlockType1:
+            return <BlockType1 data={block} />
+        case BlockTypeNumber.BlockType9:
+            return <BlockType9 data={block} />
+        case BlockTypeNumber.BlockType2M:
+            return <BlockMType2M data={block} lazyLoadImg={lazyLoadImg} />
+        case BlockTypeNumber.BlockType3M:
+            return <BlockMType3M data={block} />
+        case BlockTypeNumber.BlockType6:
+            return <BlockType6 data={block} />
+        case BlockTypeNumber.BlockType8:
+            return <BlockType8 data={block} />
+        case BlockTypeNumber.BlockType2:
+            return <>{isMobile ? <BlockType2Mobile data={block} /> : <BlockType2 data={block} />}</>
 
-//         case BlockTypeNumber.BlockType3:
-//             return <>{isMobile ? <BlockType3Mobile data={block} /> : <BlockType3 data={block} />}</>
-//         case BlockTypeNumber.BlockType4:
-//             return <>{isMobile ? <BlockType4Mobile data={block} /> : <BlockType4 data={block} />}</>
-//         case BlockTypeNumber.BlockType7:
-//             return <>{isMobile ? <BlockType7Mobile data={block} /> : <BlockType7 data={block} />}</>
-//         case BlockTypeNumber.BlockType5:
-//             return <>{isMobile ? <BlockType5Mobile data={block} /> : <BlockType5 data={block} />}</>
+        case BlockTypeNumber.BlockType3:
+            return <>{isMobile ? <BlockType3Mobile data={block} /> : <BlockType3 data={block} />}</>
+        case BlockTypeNumber.BlockType4:
+            return <>{isMobile ? <BlockType4Mobile data={block} /> : <BlockType4 data={block} />}</>
+        case BlockTypeNumber.BlockType7:
+            return <>{isMobile ? <BlockType7Mobile data={block} /> : <BlockType7 data={block} />}</>
+        case BlockTypeNumber.BlockType5:
+            return <>{isMobile ? <BlockType5Mobile data={block} /> : <BlockType5 data={block} />}</>
 
-//         default:
-//             return null
-//     }
-// }
+        default:
+            return null
+    }
+}
 
 export default function Home() {
     // // document.title = "Home"
@@ -122,11 +119,7 @@ export default function Home() {
                     {data?.dataHomeMobile?.map((block) => renderBlock(block))}
 
                     </div> */}
-                        {/* {blocksToRender?.map((block, index) => renderBlock(block, index, isMobile))} */}
-                            {
-                                            isMobile ? <HomeMob blocksToRender={blocksToRender} /> : <HomePc blocksToRender={blocksToRender} />
-                            }
-                
+                        {blocksToRender?.map((block, index) => renderBlock(block, index, isMobile))}
 
                         {/* Лениво загружаем оставшиеся блоки */}
 
