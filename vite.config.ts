1b1e328607d993faad400aa41897507777284753
vladosik1998 <vladosik4891@gmail.com>
2025-01-15 13:38:59 +0200

    add env ig

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {},
+  },
   plugins: [
     react(),
   ],
   server: {
     port: 3000,
   },
+  envDir: './',
 })
